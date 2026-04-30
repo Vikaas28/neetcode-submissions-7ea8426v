53b3b2404e007e10461941c16cd253f12f2abe2e
Vikas  sahu <vikasmangrora17@gmail.com>
2026-04-30 14:53:19 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+
+
+
+    //int t[101];
+    int solve(vector<int>& nums, int i , int n,vector<int>& t){
+        if(i>=n){
+            return 0;
+
+        }
+        if(t[i]!=-1){
+            return t[i];
+        }
+        int steal=nums[i]+solve(nums,i+2,n,t);
+        int skip =solve(nums,i+1, n,t);
+        return t[i]=  max(steal , skip );
+    }
+    int rob(vector<int>& nums) {
+        int n =nums.size();
+        vector<int>t(n,-1);
+        return solve(nums,0,n,t);
+        
+    }
+};
